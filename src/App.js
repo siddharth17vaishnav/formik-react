6f9a74525521fd6ee79a7b28e1cfe525d118c8f5
siddharth17vaishnav <siddharth17vaishnav@gmail.com>
2022-12-01 21:34:08 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Field, useFormik } from "formik";
+import * as Yup from "yup";
+import InsertLinkIcon from "@mui/icons-material/InsertLink";
+import InsertLink from "@mui/icons-material/InsertLink";
+import { useRef, useState } from "react";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 function App() {
+  const [selected, setSelected] = useState("options");
+
+  const formikSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid Email").required("Required"),
+    option: Yup.string().oneOf(["red", "blue"]).required("requried"),
+    course: Yup.string().required("Select Year"),
+  });
+  const { values, handleChange, handleSubmit, errors } = useFormik({
+    initialValues: {
+      email: "",
+      option: "",
+      course: "",
+    },
+    validationSchema: formikSchema,
+  });
+  console.log(values);
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const fileObj = event.target.files && event.target.files[0];
+    if (!fileObj) {
+      return;
+    }
+    console.log(event.target.value);
+    event.target.value = null;
+  };
+
+  const yearOptions = [{ label: 2022 }, { label: 2023 }];
+  console.log(values);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={values.email}
+          onChange={handleChange}
+          id="email"
+        />
+        <p style={{ color: "red" }}>{errors?.email}</p>
+        <TextField
+              select
+              id="course"
+              label="Course Category"              
+              margin="dense"
+              variant="outlined"
+              fullWidth
+              value={values.course}
+              onChange={handleChange}
+            >
+              {yearOptions.map(option => (
+                <MenuItem key={option.label} value={option.label}>
+                  {option.label}
+                </MenuItem>
+              ))}
+              </TextField>
+
+        <p>{errors.options}</p>
+        <button type="submit">submit</button>
+      </form>
+      <div>
+        <input
+          style={{ display: "none" }}
+          ref={inputRef}
+          type="file"
+          onChange={handleFileChange}
+        />
+        <InsertLink onClick={handleClick} />
+      </div>
     </div>
   );
 }
